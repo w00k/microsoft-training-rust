bc432e0ce5538c8a651ed6c1ef12b46c874f0644
Francisco Horacio Arenas Massa <w00kyx@localhost.localdomain>
2023-08-26 15:26:51 -0400

    add if let match pattern

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 /*
-Match, we use for control the workflow app, 
+Match, we use for control the workflow app,
 */
 fn main() {
-
     println!("Match");
 
     let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
@@ -13,5 +12,24 @@ fn main() {
             Some(fruit_name) => println!("It's a delicious {}!", fruit_name),
             None => println!("There is no fruit! :("),
         }
+    }
+
+    if_let_match();
+
+    if_let_match_one();
+}
+
+fn if_let_match() {
+    let a_number: Option<u8> = Some(7);
+    match a_number {
+        Some(7) => println!("That's my lucky number!"),
+        _ => {}
+    }
+}
+
+fn if_let_match_one() {
+    let a_number: Option<u8> = Some(7);
+    if let Some(7) = a_number {
+        println!("That's my lucky number!!!");
     }
 }
