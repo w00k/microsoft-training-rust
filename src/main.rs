86de7c3f2b704facf14532d83defe7ee3f6a09b1
Francisco Arenas <franciscoarenas.globant@latam.com>
2023-08-30 04:17:36 -0400

    derive

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,31 @@
-struct Circle {
-    radius: f64,
+use std::fmt;
+
+// Debug is for print on consola for "{:?}"
+// PartialEq is for comparison in the if "==" 
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-trait Area {
-    fn area(&self) -> f64;
-}
-
-impl Area for Circle {
-    fn area(&self) -> f64 {
-        use std::f64::consts::PI;
-        PI * self.radius.powf(2.0)
-    }
-}
-
-impl Area for Rectangle {
-    fn area(&self) -> f64 {
-        self.width * self.height
+// for p1 in println "{}", implement this function for fix it 
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
 
 fn main() {
-    let circle = Circle { radius: 5.0 };
-    let rectangle = Rectangle {
-        width: 10.0,
-        height: 20.0,
-    };
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 4, y: -3 };
 
-    println!("Circle area: {}", circle.area());
-    println!("Rectangle area: {}", rectangle.area());
+    if p1 == p2 { // can't compare two Point values!
+        println!("equal!");
+    } else {
+        println!("not equal!");
+    }
+
+    println!("{}", p1); // can't print using the '{}' format specifier!
+    println!("{:?}", p1); //  can't print using the '{:?}' format specifier!
+
 }
