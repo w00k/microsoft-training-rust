5a2193e7a6956cddabd47d9fedf36eefb0161ade
Francisco Arenas <franciscoarenas.globant@latam.com>
2023-08-30 05:11:38 -0400

    result

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
-#[derive(Debug)]
-struct Counter {
-    length: usize,
-    count: usize,
+struct Container<T> {
+    value: T,
 }
 
-impl Counter {
-    fn new(length: usize) -> Counter {
-        Counter { count: 0, length }
-    }
-}
-
-impl Iterator for Counter {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.count += 1;
-        if self.count <= self.length {
-            Some(self.count)
-        } else {
-            None
-        }
+impl<T> Container<T> {
+    pub fn new(value: T) -> Self {
+        Container { value }
     }
 }
 
 fn main() {
-    let counter = Counter::new(6);
-    println!("Counter just created: {:#?}", counter);
-
-    for number in Counter::new(10) {
-        println!("{}", number);
-    }
-
-    let sum_until_10: usize = Counter::new(10).sum();
-    assert_eq!(sum_until_10, 55);
-
-    let powers_of_2: Vec<usize> = Counter::new(8).map(|n| 2usize.pow(n as u32)).collect();
-    assert_eq!(powers_of_2, vec![2, 4, 8, 16, 32, 64, 128, 256]);
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
 }
