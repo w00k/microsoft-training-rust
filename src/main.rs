bc27df8f1947461d19d40edf732b7d443302ab39
Francisco Arenas <wook82@gmail.com>
2023-08-29 20:34:35 -0400

    generic

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
-use std::fs::File;
-use std::io::{Error, Read};
-use std::path::PathBuf;
+/*
+struct Point<T, U> {
+    x: T,
+    y: T,
+}
 
-fn read_file_contents(path: PathBuf) -> Result<String, Error> {
-    let mut string = String::new();
+x and y expect the same type value
+*/
 
-    // Access a file at a specified path
-    // ---------------------------------
-    // TODO #1:
-    // - Pass variable to `file` variable on success, or
-    // - Return from function early if there's an error
-    let mut file: File = match File::open(path) {
-        Ok(file_handle) => file_handle,
-        Err(io_error) => return Err(io_error)
-    };
-
-    // Read file contents into `String` variable with `read_to_string`
-    // ---------------------------------
-    // Success path is already filled in
-    // TODO #2: Return from function early if there's an error
-    match file.read_to_string(&mut string) {
-        Ok(_) => (),
-        Err(io_error) => return Err(io_error)
-    };
-
-    // TODO #3: Return `string` variable as expected by function signature
-    Ok(string)
+// x and y expect a different type value
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    if read_file_contents(PathBuf::from("src/main.rs")).is_ok() {
-        println!("The program found the main file.");
-    }
-    if read_file_contents(PathBuf::from("non-existent-file.txt")).is_err() {
-        println!("The program reported an error for the file that doesn't exist.");
-    }
+    let integer_and_boolean = Point { x: 5, y: false };
+    let float_and_string = Point { x: 1.0, y: "hey" };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+    let both_integer = Point { x: 10, y: 30 };
+    let both_boolean = Point { x: true, y: true };
 }
