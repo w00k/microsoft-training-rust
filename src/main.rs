7b86170738aae44a6dd9236b31a7705cdce99f73
Francisco Arenas <wook82@gmail.com>
2023-08-29 20:47:33 -0400

    without derive and the impl the compiler response two error

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
-trait Area {
-    fn area(&self) -> f64;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-struct Circle {
-    radius: f64,
-}
-
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-impl Area for Circle {
-    fn area(&self) -> f64 {
-        use std::f64::consts::PI;
-        PI * self.radius.powf(2.0)
-    }
-}
-
-impl Area for Rectangle {
-    fn area(&self) -> f64 {
-        self.width * self.height
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
 
 fn main() {
-    let circle = Circle { radius: 5.0 };
-    let rectangle = Rectangle {
-        width: 10.0,
-        height: 20.0,
-    };
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 4, y: -3 };
 
-    println!("Circle area: {}", circle.area());
-    println!("Rectangle area: {}", rectangle.area());
+    println!("{}", p1);
+    println!("{:?}", p1);
+
+    if p1 == p2 {
+        println!("equal!");
+    } else {
+        println!("not equal!");
+    }
 }
